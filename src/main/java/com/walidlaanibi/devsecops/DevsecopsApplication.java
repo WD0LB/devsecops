ad796a4225b303ad313cd8af29976328c4c08e7b
WD0LB <wd.laanibi@gmail.com>
2024-10-28 09:41:48 +0100

    Initialisation du projet

---
+++ src/main/java/com/walidlaanibi/devsecops/DevsecopsApplication.java
@@ -0,0 +1,14 @@
+package com.walidlaanibi.devsecops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsecopsApplication.class, args);
+		System.out.println("Walid Started App");
+	}
+
+}
---
+++ src/test/java/com/walidlaanibi/devsecops/DevsecopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walidlaanibi.devsecops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsecopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
